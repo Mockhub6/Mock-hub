6f7eb894d947187402b37ad4ef3885fd5aa1c088
Mockhub6 <vmonti202@gmail.com>
2026-07-23 23:25:26 +0530

    Refactor timer logic and remove unused code

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.querySelector("button");
+    let time = 30 * 60;
 
-  btn.addEventListener("click", function () {
-    alert("Mock Test जल्द शुरू होगा!");
-  });
+    const timer = document.createElement("h3");
+    document.body.insertBefore(timer, document.querySelector("a"));
 
-  let time = 30 * 60;
+    function updateTimer() {
+        let min = Math.floor(time / 60);
+        let sec = time % 60;
+        timer.innerHTML = "⏱ Time Left: " +
+            String(min).padStart(2, "0") + ":" +
+            String(sec).padStart(2, "0");
 
-  const timer = document.createElement("h3");
-  timer.id = "timer";
-  document.body.insertBefore(timer, btn);
-
-  function updateTimer() {
-    let min = Math.floor(time / 60);
-    let sec = time % 60;
-
-    timer.innerHTML =
-      "⏱ Time Left: " +
-      String(min).padStart(2, "0") +
-      ":" +
-      String(sec).padStart(2, "0");
-
-    if (time > 0) {
-      time--;
-    } else {
-      clearInterval(interval);
-      alert("Time Over!");
+        if (time > 0) time--;
     }
-  }
 
-  updateTimer();
-  const interval = setInterval(updateTimer, 1000);
+    updateTimer();
+    setInterval(updateTimer, 1000);
 });
