52763e921d1c9f55d533cd7258c9a4d4a3fbbb63
Mockhub6 <vmonti202@gmail.com>
2026-07-24 11:57:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-let questions = JSON.parse(localStorage.getItem("questions")) || [
+let questions;
+try {
+questions = JSON.parse(localStorage.getItem("questions")) || [
 {
 question: "भारत की राजधानी क्या है?",
 options: ["मुंबई", "दिल्ली", "चेन्नई", "जयपुर"],
